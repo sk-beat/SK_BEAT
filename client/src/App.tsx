07cbfd933929ee2e70ba3d27e1b9ad36fc6ac905
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-07-13 19:12:57 +0800

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -43,7 +43,7 @@ function App() {
             path="/login"
             element={
               <PublicRoute>
-                <PageTitle title="Loginn">
+                <PageTitle title="Login">
                   <LoginPage />
                 </PageTitle>
               </PublicRoute>

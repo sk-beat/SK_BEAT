c08dcc4353c500edaebca4903026212e4b89e802
Jaypee <patricksoriaga14@gmail.com>
2026-07-15 23:55:26 +0800

    update

--- client/src/components/Youth/shared/YouthHeader.tsx
+++ client/src/components/Youth/shared/YouthHeader.tsx
@@ -1,3 +1,10 @@
-export default function YouthHeader() {
+type YouthHeaderProps = {
+  subtitle: string;
+  title: string;
+  userInitial?: string;
+};
+
+export default function YouthHeader(props: YouthHeaderProps) {
+  void props;
   return null;
 }
